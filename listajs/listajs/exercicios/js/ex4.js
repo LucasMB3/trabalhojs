dff6a2e61617c6a1464a831142e22c4b411bafae
LucasMB3 <107430268+LucasMB3@users.noreply.github.com>
2023-04-07 16:41:47 -0300

    Add files via upload

---
+++ listajs/listajs/exercicios/js/ex4.js
@@ -0,0 +1,14 @@
+document.write(
+  "<h1><center>Os Primeiros 15 numeros da sequencia fibonacci</center></h1>"
+)
+
+n1 = 0
+n2 = 1
+
+for (var i = 0; i <= 15; i++) {
+  soma = n1 + n2
+  n1 = n2
+  n2 = soma
+  document.write("<b>Número: </b>", n1, "<br><br>")
+}
+document.write('<br> <img src="js/panhanha.png" alt="Cade?">')
